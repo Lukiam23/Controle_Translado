b5d9dd71c175ec8256761128f8f5840f7a80e66f
Lukiam23 <mscorreia95@gmail.com>
2022-02-09 07:12:02 -0300

    Finalizado

--- frontend/src/pages/ItemTranslado.jsx
+++ frontend/src/pages/ItemTranslado.jsx
@@ -1,14 +1,17 @@
 import styles from '../css/ItemTranslado.module.css'
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
+import { format } from 'date-fns'
 
 const axios = require('axios')
 
 export default function ItemTranslado({translado}){
 	return(
 		<div className={styles.itemContainer}>
-			<label>translado.data</label>
-
+			<label>{format(new Date(translado.data),'dd/MM/yyyy')}</label>
+			<label>{translado.descricao}</label>
+			<label>{translado.nome}</label>
+			<label>{translado.turno}</label>
 		</div>
 
 	);
--- frontend/src/pages/ListarTransladosPage.jsx
+++ frontend/src/pages/ListarTransladosPage.jsx
@@ -2,6 +2,7 @@ import styles from '../css/ListarTransladosPage.module.css'
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 import { useUsuarioContext } from './UsuarioContext';
+import ItemTranslado from "./ItemTranslado.jsx"
 
 const axios = require('axios')
 
@@ -15,13 +16,17 @@ export default function ListarTransladosPage(){
 			method: 'POST',
 			data: usuario
 		});
-		setTranslados(response.data)
+		const sortTranslados = response.data.sort((t1,t2) => (new Date(t1.data) - new Date(t2.data)));
+		const itens = sortTranslados.map(translado => <ItemTranslado key={translado.transladoid} translado={translado}/>)
+		setTranslados(itens)
 	},[]);
 
 
 	return (
-		<div className={styles.Container}>
-			
+		<div className={styles.ContainerCenter}>
+			<div className={styles.ContainerContent}>
+				{translados}
+			</div>
 		</div>
 	);
 }
